d6d12e4cd5b6d52b87fd0dbb01a91ddecbf45fa3
ivanbenko <bivans209@gmail.com>
2019-07-30 00:02:46 +0200

    added build scripts

--- build.fsx
+++ build.fsx
@@ -1,3 +1,7 @@
+#r "paket:
+nuget Fake.DotNet.Cli
+nuget Fake.IO.FileSystem
+nuget Fake.Core.Target //"
 #load ".fake/build.fsx/intellisense.fsx"
 open Fake.Core
 open Fake.DotNet
@@ -6,15 +10,50 @@ open Fake.IO.FileSystemOperators
 open Fake.IO.Globbing.Operators
 open Fake.Core.TargetOperators
 
+let artifactsPath = Path.getFullName ".artifacts"
+let publishPath = artifactsPath @@ "publish"
+let appProjects = !! "src/app/**/*.*proj"
+let testProjects = !! "src/test/**/*.*proj"
+
 Target.create "Clean" (fun _ ->
-    !! "src/**/bin"
-    ++ "src/**/obj"
-    |> Shell.cleanDirs 
+  !! "src/**/bin"
+  ++ "src/**/obj"
+  ++ artifactsPath
+  |> Shell.cleanDirs
+
+  Directory.ensure(artifactsPath)
 )
 
 Target.create "Build" (fun _ ->
-    !! "src/**/*.*proj"
-    |> Seq.iter (DotNet.build id)
+  appProjects
+  |> Seq.iter (DotNet.build id)
+)
+
+Target.create "Test" (fun _ -> 
+  testProjects
+  |> Seq.iter (DotNet.test id)
+)
+
+Target.create "Pack" (fun _ ->
+  Directory.ensure publishPath
+
+  appProjects
+  |> Seq.iter (DotNet.pack (fun opt -> 
+    { opt with 
+        OutputPath = Some publishPath
+    } 
+  ))
+)
+
+Target.create "Publish" (fun _ ->
+  !! (publishPath @@ "*.nupkg")
+  |> Seq.iter (DotNet.nugetPush (fun opt -> 
+    opt.WithPushParams(
+      { opt.PushParams with 
+          ApiKey = Some (Environment.environVarOrFail "NUGET_API_KEY")
+          Source = Some "https://api.nuget.org/v3/index.json"
+      })
+  ))
 )
 
 Target.create "All" ignore
@@ -22,5 +61,13 @@ Target.create "All" ignore
 "Clean"
   ==> "Build"
   ==> "All"
+
+"Clean"
+  ==> "Test"
+  ==> "All"
+
+"Clean"
+  ==> "Pack"
+  ==> "Publish"
 
 Target.runOrDefault "All"
